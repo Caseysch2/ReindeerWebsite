d336fe85b672e71d899c12fe481c614266588a7b
Caseysch2 <schaertlcl@gmail.com>
2019-12-29 18:50:52 -0800

    Change Jan 1st title
    Now that I know automatically getting events works, change it

--- javascript/eventprettifier.js
+++ javascript/eventprettifier.js
@@ -2,7 +2,7 @@ window.onload = function() {
 	// The list of events (all fields are optional)
     events = [
 		{
-			"title": "January 1st, Bring in the New Year with the Reindeer!",
+			"title": "Bring in the New Year with the Reindeer!",
 			"date": "01/01/2020",
 			"picture": "./images/events/2020-01-01.jpg",
 			"link": "https://www.facebook.com/events/1726163650851793/",
